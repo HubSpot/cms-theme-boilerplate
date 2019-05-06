af8f6ed17973137e6655d6d34690f7b7c5cf10d9
Jon McLaren <jon.m@spingroup.com>
2019-05-06 15:56:48 -0500

    add themejs

---
+++ CrankShaft/code/js/theme.js
@@ -0,0 +1,12 @@
+/**
+ * * /theme.js
+ * * Your Javascript goes in this file. No Javascript that is required by CrankShaft's Core is included here.
+ * TODO: Add file location to this comment, to make it easy to find when inspecting on front end.
+ */
+
+$(function() {
+    //Your code here.
+
+
+});
+/* /theme.js */
