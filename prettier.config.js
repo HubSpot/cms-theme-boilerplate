828f5dc29f51a546863cf6bf063ae83a73d6fde1
Gregory Cornelius <gcornelius@hubspot.com>
2019-09-02 17:40:51 -0400

    Add eslint and prettier and cleanup javascript

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  printWidth: 80,
+  proseWrap: 'never',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+};
