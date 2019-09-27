67969df4cfcbe88e4e315a4b90d70ecf07da0fec
Axel Simms <mail@axelsimms.com>
2019-09-27 14:03:36 -0400

    added a styled language picker to the header (#38)
    * added a styled language picker to the header
    
    * added hover effects to the pseudoelement triangle
    
    * added a current language label to the label picker for clarity
    
    * adjusted the header layout to the new design concept
    
    * fixed search bar styles and position
    
    * fixed the hoverable area of the language switcher dropdown
    
    * hide all html markup for language switcher on single languge pages
    
    * removed underline active state on mobile
    
    * moved the mobile nav into an absolute element
    
    * added toggle elements + symbols for the mobile language picker and search
    
    * added logic for showing and hiding mobile menus and some rudimentary styling
    
    * styled mobile views for language switcher and search
    
    * added better logic in the mobile header layout for hiding and showing menu options
    
    * vertically centered the desktop logo in its own column
    
    * added labels to the mobile menu elements
    
    * hid the language switcher toggle w/ FACTS and LOGIC. mostly logic.
    
    * adjusted mobile header spacing
    
    * re-adjusted mobile header spacing

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,21 @@
   var HIDE_FOCUS_STYLES_CLASS = 'disable-focus-styles';
   var SHOW_FOCUS_STYLES_CLASS = 'enable-focus-styles';
 
-  var searchBar = document.querySelector(".header__search .hs-search-field__input")
+  var firstLanguageSwitcherItem = document.querySelector(".header__language-switcher .lang_list_class li:first-child")
+  var languageSwitcherList = document.querySelector(".header__language-switcher .lang_list_class")
+
+  var Nav = document.querySelector(".header__navigation")
+  var LangSwitcher = document.querySelector(".header__language-switcher")
+  var Search = document.querySelector(".header__search")
+  var HeaderElements = document.querySelectorAll(".header--element")
+
+  var allToggles = document.querySelectorAll(".header--toggle")
+  var navToggle = document.querySelector(".header__navigation--toggle")
+  var langToggle = document.querySelector(".header__language-switcher--toggle")
+  var searchToggle = document.querySelector(".header__search--toggle")
+  var closeToggle = document.querySelector(".header__close--toggle")
+
+  var allElements = document.querySelectorAll(".header--element, .header--toggle" )
 
   function domReady(callback) {
     if (['interactive', 'complete'].indexOf(document.readyState) >= 0) {
@@ -22,12 +36,55 @@
     document.body.classList.remove(SHOW_FOCUS_STYLES_CLASS);
   }
 
-  function showActiveSearchBar(e) {
-    if (e.target.value === "") {
-      searchBar.classList.remove('active')
-    } else {
-      searchBar.classList.add('active')
-    }
+  // adds hover effects to the pseduoelement triangle on the menu
+  // for design continutity
+  function hoverLanguageSwitcher() {
+    languageSwitcherList.classList.add("first-active")
+  }
+
+  function unhoverLanguageSwitcher() {
+    languageSwitcherList.classList.remove("first-active")
+  }
+
+  function toggleNav() {
+    allToggles.forEach((toggle) => {
+      toggle.classList.toggle('hide')
+    })
+
+    Nav.classList.toggle('open')
+    navToggle.classList.toggle('open')
+
+    closeToggle.classList.toggle('show')
+  }
+
+  function toggleLang() {
+    allToggles.forEach((toggle) => {
+      toggle.classList.toggle('hide')
+    })
+
+    LangSwitcher.classList.toggle('open')
+    langToggle.classList.toggle('open')
+
+    closeToggle.classList.toggle('show')
+  }
+
+  function toggleSearch() {
+    allToggles.forEach((toggle) => {
+      toggle.classList.toggle('hide')
+    })
+
+    Search.classList.toggle('open')
+    searchToggle.classList.toggle('open')
+
+    closeToggle.classList.toggle('show')
+  }
+
+  function closeAll() {
+    allElements.forEach((element) => {
+      element.classList.remove('hide', 'open')
+    })
+
+    closeToggle.classList.remove('show')
   }
 
   domReady(function() {
@@ -42,8 +99,21 @@
       document.body.addEventListener('mousedown', hideFocusOutline);
       document.body.addEventListener('mouseup', hideFocusOutline);
 
-      // Show search outline when text exists in the search bar
-      searchBar.addEventListener('input', showActiveSearchBar);
+      // function dependent on the language switcher component
+      if (LangSwitcher) {
+        // Adds a hover style class to the parent element when the cursor hovers
+        // over the first child item
+        firstLanguageSwitcherItem.addEventListener('mouseover', hoverLanguageSwitcher);
+        firstLanguageSwitcherItem.addEventListener('mouseout', unhoverLanguageSwitcher);
+
+        // Toggles the language switcher
+        langToggle.addEventListener('click', toggleLang);
+      }
+
+      // Toggles the mobile views for menu, search, and close button
+      navToggle.addEventListener('click', toggleNav);
+      searchToggle.addEventListener('click', toggleSearch);
+      closeToggle.addEventListener('click', closeAll);
     }
   });
 })();
