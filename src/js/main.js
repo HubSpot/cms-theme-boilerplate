b7ff07941c98e9e5440b429058d283ced865e6fc
Jason Rosa <jrosa@hubspot.com>
2020-12-10 17:32:34 -0500

    Clean up remaining JS file

--- src/js/main.js
+++ src/js/main.js
@@ -9,27 +9,22 @@
     };
   }
 
-  var firstLanguageSwitcherItem = document.querySelector(
-    '.header__language-switcher .lang_list_class li:first-child'
-  );
-  var languageSwitcherList = document.querySelector(
-    '.header__language-switcher .lang_list_class'
-  );
-
-  var Nav = document.querySelector('.header__navigation');
-  var LangSwitcher = document.querySelector('.header__language-switcher');
-  var Search = document.querySelector('.header__search');
-
+  // Variables
+  var nav = document.querySelector('.header__navigation');
+  var langSwitcher = document.querySelector('.header__language-switcher');
+  var search = document.querySelector('.header__search');
   var allToggles = document.querySelectorAll('.header--toggle');
   var navToggle = document.querySelector('.header__navigation--toggle');
   var langToggle = document.querySelector('.header__language-switcher--toggle');
   var searchToggle = document.querySelector('.header__search--toggle');
   var closeToggle = document.querySelector('.header__close--toggle');
-
   var allElements = document.querySelectorAll(
     '.header--element, .header--toggle'
   );
 
+  // Functions
+
+  // Function for executing code on document ready
   function domReady(callback) {
     if (['interactive', 'complete'].indexOf(document.readyState) >= 0) {
       callback();
@@ -38,49 +33,43 @@
     }
   }
 
-  // adds hover effects to the pseduoelement triangle on the menu
-  // for design continutity
-  function hoverLanguageSwitcher() {
-    languageSwitcherList.classList.add('first-active');
-  }
-
-  function unhoverLanguageSwitcher() {
-    languageSwitcherList.classList.remove('first-active');
-  }
-
+  // Function for toggling mobile navigation
   function toggleNav() {
     allToggles.forEach(function (toggle) {
       toggle.classList.toggle('hide');
     });
 
-    Nav.classList.toggle('open');
+    nav.classList.toggle('open');
     navToggle.classList.toggle('open');
 
     closeToggle.classList.toggle('show');
   }
 
+  // Function for toggling mobile language selector
   function toggleLang() {
     allToggles.forEach(function (toggle) {
       toggle.classList.toggle('hide');
     });
 
-    LangSwitcher.classList.toggle('open');
+    langSwitcher.classList.toggle('open');
     langToggle.classList.toggle('open');
 
     closeToggle.classList.toggle('show');
   }
 
+  // Function for toggling mobile search field
   function toggleSearch() {
     allToggles.forEach(function (toggle) {
       toggle.classList.toggle('hide');
     });
 
-    Search.classList.toggle('open');
+    search.classList.toggle('open');
     searchToggle.classList.toggle('open');
 
     closeToggle.classList.toggle('show');
   }
 
+  // Function for the header close option on mobile
   function closeAll() {
     allElements.forEach(function (element) {
       element.classList.remove('hide', 'open');
@@ -89,24 +78,14 @@
     closeToggle.classList.remove('show');
   }
 
+
   domReady(function () {
     if (!document.body) {
       return;
     } else {
 
       // function dependent on the language switcher component
-      if (LangSwitcher) {
-        // Adds a hover style class to the parent element when the cursor hovers
-        // over the first child item
-        firstLanguageSwitcherItem.addEventListener(
-          'mouseover',
-          hoverLanguageSwitcher
-        );
-        firstLanguageSwitcherItem.addEventListener(
-          'mouseout',
-          unhoverLanguageSwitcher
-        );
-
+      if (langSwitcher) {
         // Toggles the language switcher
         langToggle.addEventListener('click', toggleLang);
       }
@@ -117,4 +96,5 @@
       closeToggle.addEventListener('click', closeAll);
     }
   });
+
 })();
