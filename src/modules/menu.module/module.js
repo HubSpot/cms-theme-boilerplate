c873b91c55d6aa5d225d86db5e92426b72884fca
Jason Rosa <jrosa@hubspot.com>
2021-08-02 14:55:13 -0400

    SChanging classes used for menu to use BEM

--- src/modules/menu.module/module.js
+++ src/modules/menu.module/module.js
@@ -1,17 +1,46 @@
-(function () {
-  // Global variables
-  var parentMenuItems = document.querySelectorAll(
-    '.navigation-primary li.has-submenu'
-  );
+// Website header variables
 
-  // Adds focus event listener on parent menu items so keyboard users can tab through menu
-  Array.prototype.forEach.call(parentMenuItems, function (el) {
-    el.addEventListener('focusin', function () {
-      el.classList.add('focus');
+var menuParentItems = document.querySelectorAll('.menu .menu__item--has-submenu');
+var childToggle = document.querySelectorAll('.menu .menu__child-toggle');
+
+if (menuParentItems) {
+  Array.prototype.forEach.call(menuParentItems, function(el){
+
+    // Link variables
+
+    var childToggle = el.querySelector('.menu__child-toggle');
+
+    // Handles hover over
+
+    el.addEventListener('mouseover', function(){
+      this.classList.add('menu__item--open');
+      this.querySelector('a').setAttribute('aria-expanded', 'true');
+      this.querySelector('button').setAttribute('aria-expanded', 'true');
     });
 
-    el.addEventListener('focusout', function () {
-      el.classList.remove('focus');
+    // Handles hover out
+
+    el.addEventListener('mouseout', function(){
+      document.querySelector('.menu__item--open > a').setAttribute('aria-expanded', 'false');
+      document.querySelector('.menu__item--open > button').setAttribute('aria-expanded', 'false');
+      document.querySelector('.menu__item--open').classList.remove('menu__item--open');
     });
+
+    // Handles toggle for submenus
+
+    childToggle.addEventListener('click', function(){
+      if (this.parentNode.classList.contains('menu__item--open')) {
+        this.parentNode.classList.remove('menu__item--open');
+        this.parentNode.querySelector('a').setAttribute('aria-expanded', 'false');
+        this.parentNode.querySelector('button').setAttribute('aria-expanded', 'false');
+      }
+      else {
+        this.parentNode.classList.add('menu__item--open');
+        this.parentNode.querySelector('a').setAttribute('aria-expanded', 'true');
+        this.parentNode.querySelector('button').setAttribute('aria-expanded', 'true');
+      }
+    });
+
   });
-})();
+
+}
