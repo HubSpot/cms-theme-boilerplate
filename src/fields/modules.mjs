f839aedb0420f5c78e83de8f58877fa2c5952ca9
ahmad <ahmad@hubbase.io>
2023-04-11 01:35:06 +0500

    update global fields and add style group in each module

--- src/fields/modules.mjs
+++ src/fields/modules.mjs
@@ -1,109 +1,114 @@
-const textField = {
-    required: true,
-    locked: false,
-    help_text: "",
-    inline_help_text: "",
-    name: "text",
-    label: "Text",
-    type: "text",
-    default: "Text"
-};
-
-const descriptionField = {
-    required: true,
-    locked: false,
-    help_text: "",
-    inline_help_text: "",
-    name: "description",
-    label: "Description",
-    type: "richtext",
-    default: "Description"
-};
-
-const styleGroupField = {
-    "label": "Styles",
-    "name": "styles",
-    "type": "group",
-    "tab": "STYLE",
-    "children": [
-      {
-        "label": "Text",
-        "name": "text",
-        "type": "group",
-        "children": [
-          {
-            "label": "Font",
-            "name": "font",
-            "type": "font"
+const backgroundControlls = {
+      // Background controlls starts
+      "label": "Background Controlls",
+      "name": "background_controlls",
+      "type": "group",
+      "children": [
+         {
+           "type": "gradient",
+           "id": "f451a25d-8f1a-b01b-e405-e3051acb6491",
+           "label": "Gradient",
+           "name": "gradient_field",
+           "default": {
+            "colors": [
+             {
+              "color": {
+               "r": 255,
+               "g": 255,
+               "b": 255,
+               "a": 0
+              }
+             },
+             {
+              "color": {
+               "r": 0,
+               "g": 0,
+               "b": 0,
+               "a": 0
+              }
+             }
+            ],
+            "side_or_corner": {
+     "verticalSide": "BOTTOM",
+     "horizontalSide": null
+    }
+           }
+         },
+         {
+           "type": "backgroundimage",
+           "id": "ad86dee6-fff8-f88f-cbd0-e0d931200b21",
+           "label": "Background image",
+           "name": "backgroundimage_field"
           }
+          
         ]
-      },
-      {
-        "label": "Background",
-        "name": "background",
-        "type": "group",
-        "children": [
-          {
-            "label": "Color",
-            "name": "color",
-            "type": "color"
-          }
-        ]
-      },
-      {
-        "label": "Border",
-        "name": "border",
-        "type": "group",
-        "children": [
-          {
-            "label" : "Border",
-            "name" : "border",
-            "type" : "border"
-          }
-        ]
-      },
-      {
-        "label": "Corner",
-        "name": "corner",
-        "type": "group",
-        "children": [
-          {
-            "label": "Radius",
-            "name": "radius",
-            "type": "number",
-            "display": "text",
-            "max": 100,
-            "step": 1,
-            "suffix": "px"
-          }
-        ]
-      },
-      {
-        "label": "Spacing",
-        "name": "spacing",
-        "type": "group",
-        "children": [
-          {
-            "label": "Spacing",
-            "name": "spacing",
-            "type": "spacing"
-          }
-        ]
-      },
-      {
-        "label": "Alignment",
-        "name": "alignment",
-        "type": "group",
-        "children": [
-          {
-            "label": "Alignment",
-            "name": "alignment",
-            "type": "alignment",
-            "alignment_direction": "HORIZONTAL"
-          }
-        ]
-      }
-    ]
+      // Background controlls ends
+    
   }
 
-export { textField, descriptionField, styleGroupField };
+const spacingControlls = {
+  
+      // spacing group starts
+      "label": "Spacing Controlls",
+      "name": "spacing_controlls",
+      "type": "group",
+      "children": [
+         {
+           "type": "spacing",
+           "id": "b38d0190-9bde-6955-b2cd-19acdb9a0752",
+           "label": "Spacing for Desktop",
+           "name": "spacing_for_desktop",
+           "default": {
+            "padding": {
+              "top": {
+               "value": 80,
+               "units": "px"
+              },
+              "bottom": {
+               "value": 80,
+               "units": "px"
+              },
+              "left": {
+               "value": null,
+               "units": null
+              },
+              "right": {
+               "value": null,
+               "units": null
+              }
+             }
+           }
+          },
+          {
+           "type": "spacing",
+           "id": "b38d0190-9bde-6955-c983-19acdb9a0752",
+           "label": "Spacing for Mobile",
+           "name": "spacing_for_Mobile",
+           "default" : {
+             "padding": {
+              "top": {
+               "value": 40,
+               "units": "px"
+              },
+              "bottom": {
+               "value": 40,
+               "units": "px"
+              },
+              "left": {
+               "value": null,
+               "units": null
+              },
+              "right": {
+               "value": null,
+               "units": null
+              }
+             }
+           }
+          }
+        ]// spacing group starts
+     
+ 
+}
+
+
+export {backgroundControlls, spacingControlls };
--- src/modules/text.module/fields.mjs
+++ src/modules/text.module/fields.mjs
@@ -1,9 +1,27 @@
-import { textField, styleGroupField } from '../../fields/modules.mjs';
+import { spacingControlls, backgroundControlls } from '../../fields/modules.mjs';
 
 const fields = (fieldOptions) => {
 	return [
-		textField,
-		styleGroupField,
+	    // content group starts 
+	    [
+			{
+			 "type": "richtext",
+			 "id": "d7780957-71b1-9366-6e75-344e05e4556b",
+			 "label": "Rich text",
+			 "name": "richtext_field",
+			 "default": "<h2>What is Lorem Ipsum?</h2>\n<p><strong>Lorem Ipsum</strong><span>&nbsp;</span>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.<span>&nbsp;</span></p>"
+			}
+		   ],// content group ends 
+		{
+			"label": "Styles",
+			"name": "styles",
+			"type": "group",
+			"tab": "STYLE",
+			"children": [
+				backgroundControlls,
+				spacingControlls
+				]
+		}
 	]
 };
 
