9c00144f25f909b65e72cffa56ac560cfaa9712c
coreyt-gremlin <55258574+coreyt-gremlin@users.noreply.github.com>
2024-08-08 12:13:21 -0400

    Setup + Secrets

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+'./modules/**/*.{html, css, js}',
+'./templates/**/*.{html, css, js}',
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
